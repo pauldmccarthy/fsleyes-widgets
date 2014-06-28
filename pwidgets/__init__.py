74b3dc78a1f1c6d5ea75fd21ac1c5023a7cdac5f
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-06-28 15:39:15 +0100

    widgets.py documentation. Also, a short comment for the pwidgets package.

---
+++ pwidgets/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+# __init__.py - Custom wx widgets used by the props package.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""Various custom :mod:`wx` widgets used by the props package."""
