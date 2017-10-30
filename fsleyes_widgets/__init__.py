c50f80c6e078b8685c4d70e55c4cb969961eff25
Paul McCarthy <pauldmccarthy@gmail.com>
2017-10-30 16:43:53 +0000

    Moved fsleyes_widgets functions into utils package

--- fsleyes_widgets/__init__.py
+++ fsleyes_widgets/__init__.py
@@ -14,73 +14,14 @@ my own implementations to work around annoying, quirky, and/or downright buggy
 behaviour in the existing controls.
 
 
-This file is used to store the current ``fsleyes-widgets`` version, and also
-contains a handful of utility functions:
-
-.. autosummary::
-   :nosignature::
-
-   wxversion
-   isalive
+This file is used to store the current ``fsleyes-widgets`` version.
 """
 
 
 __version__ = '0.2.0.dev'
 
 
-
-WX_PYTHON  = 1
-"""Constant returned by the ``wxversion`` function, indicating that wxPython
-3.0.2.0 or older is being used.
-"""
-
-
-WX_PHOENIX = 2
-"""Constant returned by the ``wxversion`` function, indicating that
-wxPython/Phoenix (>= 3.0.3) is being used.
-"""
-
-
-def wxversion():
-    """Determines the version of wxPython being used. Returns either ``WX_PYTHON``
-    or ``WX_PHOENIX``.
-    """
-    import wx
-
-    pi        = [t.lower() for t in wx.PlatformInfo]
-    isPhoenix = False
-
-    for tag in pi:
-        if 'phoenix' in tag:
-            isPhoenix = True
-            break
-
-    if isPhoenix: return WX_PHOENIX
-    else:         return WX_PYTHON
-
-
-def isalive(widget):
-    """Returns ``True`` if the given ``wx.Window`` object is "alive" (i.e.  has
-    not been destroyed), ``False`` otherwise. Works in both wxPython and
-    wxPython/Phoenix.
-
-    .. warning:: Don't try to test whether a ``wx.MenuItem`` has been
-                 destroyed, as it will probably result in segmentation
-                 faults. Check the parent ``wx.Menu`` instead.
-    """
-
-    import wx
-
-    wxver = wxversion()
-
-    if wxver == WX_PHOENIX:
-        excType = RuntimeError
-    elif wxver == WX_PYTHON:
-        excType = wx.PyDeadObjectError
-
-    try:
-        widget.GetParent()
-        return True
-
-    except excType:
-        return False
+from fsleyes_widgets.utils import (WX_PYTHON,
+                                   WX_PHOENIX,
+                                   wxversion,
+                                   isalive)
--- fsleyes_widgets/utils/__init__.py
+++ fsleyes_widgets/utils/__init__.py
@@ -4,6 +4,71 @@
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
-"""This module contains a collection of small utility modules for doing random
-things.
+"""This package contains a collection of small utility modules for doing random
+things. A few functions are also defined at the package level:
+
+.. autosummary::
+   :nosignatures:
+
+    wxversion
+    isalive
 """
+
+
+
+WX_PYTHON  = 1
+"""Constant returned by the ``wxversion`` function, indicating that wxPython
+3.0.2.0 or older is being used.
+"""
+
+
+WX_PHOENIX = 2
+"""Constant returned by the ``wxversion`` function, indicating that
+wxPython/Phoenix (>= 3.0.3) is being used.
+"""
+
+
+def wxversion():
+    """Determines the version of wxPython being used. Returns either ``WX_PYTHON``
+    or ``WX_PHOENIX``.
+    """
+
+    import wx
+
+    pi        = [t.lower() for t in wx.PlatformInfo]
+    isPhoenix = False
+
+    for tag in pi:
+        if 'phoenix' in tag:
+            isPhoenix = True
+            break
+
+    if isPhoenix: return WX_PHOENIX
+    else:         return WX_PYTHON
+
+
+def isalive(widget):
+    """Returns ``True`` if the given ``wx.Window`` object is "alive" (i.e.  has
+    not been destroyed), ``False`` otherwise. Works in both wxPython and
+    wxPython/Phoenix.
+
+    .. warning:: Don't try to test whether a ``wx.MenuItem`` has been
+                 destroyed, as it will probably result in segmentation
+                 faults. Check the parent ``wx.Menu`` instead.
+    """
+
+    import wx
+
+    wxver = wxversion()
+
+    if wxver == WX_PHOENIX:
+        excType = RuntimeError
+    elif wxver == WX_PYTHON:
+        excType = wx.PyDeadObjectError
+
+    try:
+        widget.GetParent()
+        return True
+
+    except excType:
+        return False
