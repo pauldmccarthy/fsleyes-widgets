0ea09458d58668791ca18d50fdbf79cc57257327
Paul McCarthy <pauldmccarthy@gmail.com>
2017-11-24 11:14:22 +1030

    Moved all CI scripts into separate files. Also running tests on wxpython 3. Not sure if they're going to pass.

---
+++ .ci/build_doc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python setup.py doc
+mv doc/html doc/"$CI_COMMIT_REF_NAME"
---
+++ .ci/test_template.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Install $PY_VERSION, xvfb, and all
+# of the wxpython dependencies.
+apt-get update -y || true
+apt-get install -y software-properties-common python-software-properties xvfb libgtk2.0-0 libnotify4 freeglut3 libsdl1.2debian
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update -y || true
+apt-get install -y $PY_VERSION "$PY_VERSION"-dev $PY_PACKAGES
+$PY_VENV test.venv
+source test.venv/bin/activate
+pip install --upgrade pip setuptools
+
+# If running on a fork repository, we merge in the
+# upstream/master branch. This is done so that merge
+# requests from fork to the parent repository will
+# have unit tests run on the merged code, something
+# which gitlab CE does not currently do for us.
+if [[ "$CI_PROJECT_PATH" != "$UPSTREAM_PROJECT" ]]; then
+  git fetch upstream;
+  git merge --no-commit --no-ff upstream/master;
+fi;
+
+$INSTALL_WX
+
+# All other deps can be installed as normal.
+# We install test dependenciesd through pip,
+# because if we let setuptools do it, it
+# will build/install everything from source,
+# rather than using wheels.
+pip install -r requirements.txt
+pip install sphinx sphinx-rtd-theme
+pip install pytest pytest-cov pytest-html pytest-runner mock coverage
+
+
+# style stage
+if [ "$TEST_STYLE"x != "x" ]; then pip install pylint flake8; fi;
+if [ "$TEST_STYLE"x != "x" ]; then flake8                           fsleyes_widgets || true; fi;
+if [ "$TEST_STYLE"x != "x" ]; then pylint --output-format=colorized fsleyes_widgets || true; fi;
+if [ "$TEST_STYLE"x != "x" ]; then exit 0; fi
+
+# Run the tests.
+xvfb-run python setup.py test
