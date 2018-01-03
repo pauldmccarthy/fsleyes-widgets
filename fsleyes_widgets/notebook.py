105d565657d704dd1f6530321545558f4b883633
Paul McCarthy <pauldmccarthy@gmail.com>
2018-01-03 17:34:48 +0000

    Notebook allows style and border to be customised.

--- fsleyes_widgets/notebook.py
+++ fsleyes_widgets/notebook.py
@@ -24,16 +24,22 @@ class Notebook(wx.Panel):
     toggling of page visibility.
     """
 
-    def __init__(self, parent):
+    def __init__(self, parent, style=None, border=5):
         """Create a :class:`Notebook` object.
 
         :arg parent: The :mod:`wx` parent object.
+        :arg style:  Passed to ``wx.Panel.__init__``. If not provided,
+                     defaults to ``wx.SUNKEN_BORDER``.
+        :arg border: Border (in pixels) around pages. Defaults to 5.
         """
 
-        wx.Panel.__init__(self, parent, style=wx.SUNKEN_BORDER)
+        if style is None:
+            style = wx.SUNKEN_BORDER
 
+        wx.Panel.__init__(self, parent, style=style)
+
+        self.__border      = border
         self.__buttonPanel = wx.Panel(self)
-
         self.__sizer       = wx.BoxSizer(wx.VERTICAL)
         self.__buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -45,13 +51,13 @@ class Notebook(wx.Panel):
         # a row of buttons along the top
         self.__sizer.Add(
             self.__buttonPanel,
-            border=5,
+            border=self.__border,
             flag=wx.EXPAND | wx.ALIGN_CENTER | wx.TOP | wx.RIGHT | wx.LEFT)
 
         # a horizontal line separating the buttons from the pages
         self.__sizer.Add(
             self.__dividerLine,
-            border=5,
+            border=self.__border,
             flag=wx.EXPAND | wx.ALIGN_CENTER | wx.LEFT | wx.RIGHT)
 
         # a vertical line at the start of the button row
@@ -120,7 +126,10 @@ class Notebook(wx.Panel):
         # index + 2 to account for the button panel and
         # the horizontal divider line (see __init__)
         self.__sizer.Insert(
-            index + 2, page, border=5, flag=wx.EXPAND | wx.ALL, proportion=1)
+            index + 2,
+            page,
+            border=self.__border,
+            flag=wx.EXPAND | wx.ALL, proportion=1)
 
         self.__buttonSizer.Insert(
             buttonIdx,
