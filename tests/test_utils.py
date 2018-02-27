5495dec474ecdc16f0947adc5435f1d90a0254a2
Paul McCarthy <pauldmccarthy@gmail.com>
2018-02-27 11:37:54 +0000

    Test isalive function on menu items

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,14 +16,23 @@ def test_isalive():
     run_with_wx(_test_isalive)
 
 def _test_isalive():
-    frame  = wx.GetApp().GetTopWindow()
-    sizer  = wx.BoxSizer(wx.VERTICAL)
-    panel  = wx.Panel(frame)
-    child1 = wx.Panel(panel)
-    child2 = wx.Panel(panel)
+    frame   = wx.GetApp().GetTopWindow()
+    menuBar = wx.MenuBar()
+    menu1   = wx.Menu()
+    menu2   = wx.Menu()
+    menu3   = wx.Menu()
+    sizer   = wx.BoxSizer(wx.VERTICAL)
+    panel   = wx.Panel(frame)
+    child1  = wx.Panel(panel)
+    child2  = wx.Panel(panel)
 
     sizer.Add(panel, flag=wx.EXPAND, proportion=1)
     frame.SetSizer(sizer)
+    frame.SetMenuBar(menuBar)
+
+    menuBar        .Append(       menu1, 'menu1')
+    menuBar        .Append(       menu2, 'menu2')
+    menu3id = menu2.AppendSubMenu(menu3, 'menu3').GetId()
 
     sizer = wx.BoxSizer(wx.VERTICAL)
     sizer.Add(child1, flag=wx.EXPAND, proportion=1)
@@ -43,6 +52,12 @@ def _test_isalive():
 
     assert not fw.isalive(child1)
     assert     fw.isalive(child2)
+
+    menu2  .Remove(menu3id)
+    menuBar.Remove( 1)
+
+    assert     fw.isalive(menu1)
+    assert not fw.isalive(menu3)
 
 
 def test_wxversion():
