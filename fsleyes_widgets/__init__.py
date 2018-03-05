6e10b8c332f2c4a0bd382c0f8e374b34d8c6b738
Paul McCarthy <pauldmccarthy@gmail.com>
2018-03-05 16:34:09 +0000

    bump version

--- fsleyes_widgets/__init__.py
+++ fsleyes_widgets/__init__.py
@@ -18,7 +18,7 @@ This file is used to store the current ``fsleyes-widgets`` version.
 """
 
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 
 from fsleyes_widgets.utils import (WX_PYTHON,  # noqa
