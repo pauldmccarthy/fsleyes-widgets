a5ca27c2aba66606040896026626c3afea8ae87e
Paul McCarthy <pauldmccarthy@gmail.com>
2018-10-13 13:51:30 +0100

    MNT: Version

--- fsleyes_widgets/__init__.py
+++ fsleyes_widgets/__init__.py
@@ -18,7 +18,7 @@ This file is used to store the current ``fsleyes-widgets`` version.
 """
 
 
-__version__ = '0.6.5'
+__version__ = '0.6.6'
 
 
 from fsleyes_widgets.utils import (WX_PYTHON,  # noqa
