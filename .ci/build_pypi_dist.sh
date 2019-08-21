69c5a8fffe34b3513cb8f9c78d64b3f132411bb5
Paul McCarthy <pauldmccarthy@gmail.com>
2019-08-21 13:22:41 +0100

    CI: Test building/installing pypi dist

--- .ci/build_pypi_dist.sh
+++ .ci/build_pypi_dist.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-pip install wheel
+set -e
+
+source /test.venv/bin/activate
+
+pip install wheel setuptools twine
 python setup.py sdist
 python setup.py bdist_wheel
+twine check dist/*
+
+PIPARGS="--retries 10 --timeout 30"
+
+pip install dist/*.whl
+pip uninstall -y fsleyes-widgets
+
+pip install dist/*.tar.gz
+pip uninstall -y fsleyes-widgets
