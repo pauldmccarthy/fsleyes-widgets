80fcc1e695f15cd6f6ba61b0e5bdc92f09b43fbf
Paul McCarthy <pauldmccarthy@gmail.com>
2021-02-18 10:42:33 +0000

    ENH: Migrate wx/GUI functions over from fslpy into fsleyes-widgets

--- fsleyes_widgets/__init__.py
+++ fsleyes_widgets/__init__.py
@@ -22,6 +22,21 @@ __version__ = '0.11.0.dev0'
 
 
 from fsleyes_widgets.utils import (WX_PYTHON,  # noqa
-                                   WX_PHOENIX,
+                                   WX_PHOENIX
+                                   WX_UNKNOWN,
+                                   WX_MAC_COCOA,
+                                   WX_MAC_CARBON,
+                                   WX_GTK,
                                    wxversion,
+                                   wxVersion,
+                                   wxFlavour,
+                                   wxPlatform,
+                                   frozen,
+                                   canHaveGui,
+                                   haveGui,
+                                   inSSHSession,
+                                   inVNCSession,
+                                   glVersion,
+                                   glRenderer,
+                                   glIsSoftwareRenderer,
                                    isalive)
