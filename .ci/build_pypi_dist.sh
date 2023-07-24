d302ee8943051fbb3ecc895500743a6c514997d2
Paul McCarthy <pauldmccarthy@gmail.com>
2023-07-24 18:12:29 +0100

    CI: remove cruft

--- .ci/build_pypi_dist.sh
+++ .ci/build_pypi_dist.sh
@@ -4,9 +4,8 @@ set -e
 
 source /test.venv/bin/activate
 
-pip install --upgrade pip wheel setuptools twine
-python setup.py sdist
-python setup.py bdist_wheel
+pip install --upgrade pip wheel setuptools twine build
+python -m build
 twine check dist/*
 
 PIPARGS="--retries 10 --timeout 30"
