ede0f25ed421517ddc43e6515e4ae6a3a277c8bf
Paul McCarthy <pauldmccarthy@gmail.com>
2025-11-17 13:31:32 +0000

    CI: Remove virtualenv activate call

--- .ci/build_doc.sh
+++ .ci/build_doc.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-source /test.venv/bin/activate
 pip install ".[doc]"
 sphinx-build doc public
--- .ci/test_template.sh
+++ .ci/test_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /test.venv/bin/activate
+set -e
 
 PIPARGS="--retries 10 --timeout 30"
 
