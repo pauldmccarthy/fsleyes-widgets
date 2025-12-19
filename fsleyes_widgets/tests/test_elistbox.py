8eb74b86aa5786d2a0769441a1664e7522b955ca
Paul McCarthy <pauldmccarthy@gmail.com>
2025-12-19 13:17:23 +1030

    TEST: Unit test for move/select logic when a filter is active

--- fsleyes_widgets/tests/test_elistbox.py
+++ fsleyes_widgets/tests/test_elistbox.py
@@ -47,6 +47,7 @@ def _test_create():
         assert listbox.GetItemLabel(  i) == labels[i]
         assert listbox.GetItemData(   i) == items[i]
 
+
 def test_add_remove():
     run_with_wx(_test_add_remove)
 def _test_add_remove():
@@ -75,3 +76,52 @@ def _test_add_remove():
         items.insert(idx, item)
         listbox.Insert(item, idx)
         assert list(listbox.GetLabels()) == items
+
+
+def test_hidden():
+    run_with_wx(_test_hidden)
+def _test_hidden():
+
+    class FakeEv(object):
+        def __init__(self, key):
+            self.key = key
+        def GetKeyCode(self):
+            return self.key
+        def HasModifiers(self):
+            return False
+        def Skip(self):
+            pass
+
+    frame   = wx.GetApp().GetTopWindow()
+    items   = list(range(10))
+    labels  = [str(i) for i in items]
+    listbox = elistbox.EditableListBox(
+        frame,
+        labels=labels,
+        clientData=items)
+
+    def hide(label, data):
+        return data % 2 == 0
+
+    listbox.SetSelection(0)
+    listbox.MoveItem(5)
+    assert listbox.GetSelection() == 5
+    assert listbox.GetItemData() == [1, 2, 3, 4, 5, 0, 6, 7, 8, 9]
+
+    listbox.MoveItem(-5)
+    assert listbox.GetSelection() == 0
+    assert listbox.GetItemData() == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    listbox.ApplyFilter(hide)
+    assert listbox.GetVisibleItemData() == [0, 2, 4, 6, 8]
+    assert listbox.GetSelection() == 0
+
+    listbox._onKeyboard(FakeEv(wx.WXK_DOWN))
+    assert listbox.GetSelection() == 2
+    listbox._onKeyboard(FakeEv(wx.WXK_UP))
+    assert listbox.GetSelection() == 0
+
+    listbox.MoveItem(1)
+    assert listbox.GetSelection() == 2
+    assert listbox.GetVisibleItemData() == [2, 0, 4, 6, 8]
+    assert listbox.GetItemData() == [1, 2, 0, 3, 4, 5, 6, 7, 8, 9]
